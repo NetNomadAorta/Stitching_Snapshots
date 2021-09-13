6a0f5c159be315a1d66520466cca67d06c10c2da
NetNomadAorta <30355837+NetNomadAorta@users.noreply.github.com>
2021-09-13 02:58:01 -0400

    Added Horizontal Stitching and more

--- Stitcher.py
+++ Stitcher.py
@@ -1,20 +1,26 @@
+import os
 import cv2
 import numpy as np
+import glob
 
 
 # User Parameters/Constants to Set
-MATCH_CL = 0.85 # Minimum confidence level (CL) required to match golden-image to scanned image
-SPLIT_MATCHES_CL =  0.60 # Splits MATCH_CL to SPLIT_MATCHES_CL (defects) to one folder, rest (no defects) other folder
-STICHED_IMAGES_DIRECTORY = "Images/Stitched_Images/"
-GOLDEN_IMAGES_DIRECTORY = "Images/Golden_Images/"
-SLEEP_TIME = 0.0 # Time to sleep in seconds between each window step
+MATCH_CL = 0.80 # Minimum confidence level (CL) required to match golden-image to scanned image
+SNAPSHOTS_DIRECTORY = "Images/Snapshots/"
+# STITCHED_IMAGES_DIRECTORY = "/Images/Stitched_Images/"
+
+
+def deleteDirContents(dir):
+    # Deletes photos in path "dir"
+    # # Used for deleting previous cropped photos from last run
+    for f in os.listdir(dir):
+        os.remove(os.path.join(dir, f))
 
 
 # Comparison scan window-image to golden-image
 def getMatch(window, goldenImage, x, y):
     h1, w1, c1 = window.shape
     h2, w2, c2 = goldenImage.shape
-    print(c1, c2, h1, h2, w1, w2)
     if c1 == c2 and h2 <= h1 and w2 <= w1:
         method = eval('cv2.TM_CCOEFF_NORMED')
         res = cv2.matchTemplate(window, goldenImage, method)   
@@ -30,46 +36,37 @@ def getMatch(window, goldenImage, x, y):
             return (max_loc[0], max_loc[1], max_loc[0] + w2, max_loc[1] + h2, max_val)
         
         else:
-            print("HERE")
             return ("null", "null", "null", "null", "null")
 
 
-im1 = cv2.imread("./Images/1.jpg")
-im2 = cv2.imread("./Images/2.jpg")
-im3 = cv2.imread("./Images/3.jpg")
+# MAIN():
+# =============================================================================
+# Clears some of the screen for asthetics
+print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 
-x1, y1, x2, y2, matchedCL = getMatch(im1[:,-300:,:], im2[20:-20,:100,:], 0, 0)
+# Deletes contents in Stitched-Images folder
+deleteDirContents("./Images/Stitched_Images/")
 
-newImg = np.zeros((3000, 5000, 3))
-newImg[50: 50+im1.shape[0], 50: 50+im1.shape[1], :] = im1
-newImg[50-20+y1: 50-20+y1+im2.shape[0], 50+im1.shape[1]-300+x1: 50+im1.shape[1]-300+x1+im2.shape[1]] = im2
-# :, im1.shape[1]-300+x1:im1.shape[1]-300+x2] = im2[20:-20,:100,:]
+numDir = glob.glob(SNAPSHOTS_DIRECTORY + "*")
+isFirstTime = 1
+rowNum = 1
 
-cv2.imwrite("./Images/test.jpg", newImg)
+for imagePath in glob.glob(SNAPSHOTS_DIRECTORY + "*"):
+    image = cv2.imread(imagePath)
+    if isFirstTime == 1:
+        growingImage = image
+        isFirstTime = 0
+        continue
+    
+    x1, y1, x2, y2, matchedCL = getMatch(growingImage[:,-300:,:], image[20:-20,:100,:], 0, 0)
+    if x1 != "null":
+        growingImage = np.hstack( (growingImage[:, :-300+x1+10], image[:,10:]) )
+    else: 
+        cv2.imwrite("./Images/Stitched_Images/Stitched_Image-Row_{}.jpg".format(rowNum), growingImage)
+        growingImage = image
+        rowNum += 1
+
+rowNum += 1
+cv2.imwrite("./Images/Stitched_Images/Stitched_Image-Row_{}.jpg".format(rowNum), growingImage)
 
 
-x1, y1, x2, y2, matchedCL = getMatch(im1[:,-300:,:], im2[20:-20,:100,:], 0, 0)
-
-# im2[-20+y1:, :] = im2[:-(-20+y1), :]
-# im2[:-20+y1, :] = 0
-
-newImg2 = np.hstack( (im1[:, :-300+x1+10], im2[:,10:]) )
-cv2.imwrite("./Images/test2.jpg", newImg2)
-
-
-x1, y1, x2, y2, matchedCL = getMatch(newImg2[:,-300:,:], im3[20:-20,:100,:], 0, 0)
-
-newImg3 = np.hstack( (newImg2[:, :-300+x1+10], im3[:,10:]) )
-cv2.imwrite("./Images/test3.jpg", newImg3)
-
-# image = im1.copy()
-# image = cv2.resize(image, (900, 900))
-# cv2.imshow("image", image)
-
-# im3 = np.hstack((im1, im2[:,290:,:]))
-# cv2.imwrite("./Images/test.jpg", im3)
-# cv2.imwrite("./Images/imtest.jpg", im2[:,100:,:])
-
-# imyes = im1.copy()
-# imyes = cv2.resize(im3, (900, 900))
-# cv2.imshow("Combined", imyes)
